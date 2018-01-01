be0a84a4f65935dd963140c0d8cd6d120a268065
Eryk Puławski <eryk.pulawski@pollub.edu.pl>
2018-01-01 21:04:20 +0100

    output list of Recipes display recipe details +ingredient model output shopping list +edit shopping list buttons

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'test description',
+      'https://cdn.pixabay.com/photo/2017/11/08/15/34/recipe-2930786_960_720.jpg'),
+    new Recipe('Test recipe2', 'test description2',
+      'https://cdn.pixabay.com/photo/2017/11/08/15/34/recipe-2930786_960_720.jpg')
+  ];
 
   constructor() { }
 
