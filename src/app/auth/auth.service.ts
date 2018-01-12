ce6063a136c77963496e4ec15c4adae30171855d
Eryk Puławski <eryk.pulawski@pollub.edu.pl>
2018-01-12 01:51:59 +0100

    sending authentication token checking authentication status displaying actions only if user is logged in added logout button

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
 
 export class AuthService {
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth()
       .createUserWithEmailAndPassword(email, password)
@@ -12,10 +14,30 @@ export class AuthService {
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
-        response => console.log(response)
+        response => {
+          firebase.auth().currentUser.getIdToken().then(
+            (token: string) => this.token = token
+          );
+        }
       )
       .catch(
         error => console.log(error)
       );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken().then(
+      (token: string) => this.token = token
+    );
+    return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
+
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,35 +3,38 @@ import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/add/operator/map';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http, private recipeService: RecipeService) {
+  constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {
   }
 
   storeRecipes() {
-    return this.http.put('https://recipe-book-41568.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put('https://recipe-book-41568.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://recipe-book-41568.firebaseio.com/recipes.json')
-        .map(
-          (response: Response) => {
-            const recipes: Recipe[] = response.json();
-            for (const recipe of recipes) {
-              if (!recipe['ingredients']) {
-                recipe['ingredients'] = [];
-                console.log(recipe);
-              }
+    const token = this.authService.getToken();
+    this.http.get('https://recipe-book-41568.firebaseio.com/recipes.json?auth=' + token)
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+              console.log(recipe);
             }
-            return recipes;
           }
-        )
-        .subscribe(
-      (recipes: Recipe[]) => {
-        this.recipeService.setRecipes(recipes);
-      }
-    );
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
+          this.recipeService.setRecipes(recipes);
+        }
+      );
   }
 }
